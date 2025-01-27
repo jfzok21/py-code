c19928c68db63c6e1c0a1af89c6434ac82aaa977
jfzok21 <mutejf9487@icloud.com>
2025-01-27 23:53:13 +0800

    修改 九九乘法表

--- 九九乘法表.py
+++ 九九乘法表.py
@@ -1,4 +1,7 @@
-for a in range(1,10):
-    for b in range(1,10):
-        print(f'{a} x {b} = {a*b}')
-    print(end="\n")
+path = '九九乘法表.txt'
+with open(path, 'a') as f:
+    for a in range(1,10):
+        for b in range(1,10):
+            f.write(f'|{b} x {a} = {a*b}|')
+        f.write("\n")
+print('good')
