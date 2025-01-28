4720bb6039bf7edced38c41dd0a5c08ee6731973
jfzok21 <mutejf9487@icloud.com>
2025-01-28 19:24:11 +0800

    更新 九九乘法表

--- 九九乘法表.py
+++ 九九乘法表.py
@@ -1,7 +1,12 @@
-path = '九九乘法表.txt'
-with open(path, 'a') as f:
-    for a in range(1,10):
-        for b in range(1,10):
-            f.write(f'|{b} x {a} = {a*b}|')
+path = "九九乘法表.txt"
+with open(path, 'w') as f:
+    for b in range(2, 10):
+        f.write(f'{b}        |')
+    f.write("\n")
+    f.write("".join(["-" * 9 + "|" for _ in range(2, 10)]))
+    f.write("\n")
+
+    for a in range(2, 10):
+        for b in range(2, 10):
+            f.write(f' {b}x{a}={a * b:<2}  |')
         f.write("\n")
-print('good')
